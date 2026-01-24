54d5690966361221b905af4b9a03e7e0e8de272d
michaelmdrs <mackelldeveloper@gmail.com>
2026-01-24 10:37:53 -0300

    feat: Refatorei as constantes de peso homem/mulher e utilizei um operador ternário para fazer o tratamento dos cálculos e no if seguinte exibir o resultado de acordo com o check

--- javascript/cap04/ex4_2.js
+++ javascript/cap04/ex4_2.js
@@ -8,18 +8,17 @@ addEventListener('submit', (e) => {
     const inNome = nome.value;
     const inAltura = parseFloat(altura.value);
 
-    const pesoHomem = 22 * Math.pow(inAltura, 2);
-    const pesoMulher = 21 * Math.pow(inAltura, 2);
+    const peso = masculino ? 22 * Math.pow(inAltura, 2) : 21 * Math.pow(inAltura, 2)
 
     if(masculino) {
         resultado.classList.remove('hidden');
         resultado.innerHTML = `<p class="text-2xl text-purple-900 font-bold">
-        ${inNome}: Seu peso ideal é ${pesoHomem.toFixed(2)}kg
+        ${inNome}: Seu peso ideal é ${peso.toFixed(2)}kg
         </p>`
         
     } else if (feminino) {
         resultado.innerHTML = `<p class="text-2xl text-purple-900 font-bold">
-        ${inNome}: Seu peso ideal é ${pesoMulher.toFixed(2)}kg
+        ${inNome}: Seu peso ideal é ${peso.toFixed(2)}kg
         </p>`
         
     } else {
